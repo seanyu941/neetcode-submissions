cfb79a02b61694055c2dae2211a3c348d73fbc29
seanyu941 <250726215+seanyu941@users.noreply.github.com>
2026-09-06 19:46:19 -0400

    Add: is-palindrome - submission-21

---
+++ Data Structures & Algorithms/is-palindrome/submission-21.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new = ''.join(filter(str.isalnum, s)).lower().replace(' ', '')
+        i, j = 0, len(new) - 1
+        print(new)
+        while i < j:
+            if new[i] == new[j]:
+                i += 1
+                j -= 1
+            else:
+                return False
+        return True
+        
